6e204f9cdd75be7dff727daf7473629fa92ae622
mbechi <agnessmminaa@gmail.com>
2023-10-23 23:40:27 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,20 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py <employee_id>")
+        sys.exit(1)
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    
+    try:
+        user = requests.get(url + "users/{}".format(employee_id)).json()
+        todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+
+        completed = [t.get("title") for t in todos if t.get("completed") is True]
+        print("Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), len(completed), len(todos)))
+        [print("\t {}".format(c)) for c in completed]
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
