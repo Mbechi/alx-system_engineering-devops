8ddf18e677393e1bca9670504efc6f12eacb7f44
mbechi <agnessmminaa@gmail.com>
2023-10-23 23:36:40 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,14 @@
 #!/usr/bin/python3
-"""
-Gather data from a REST API for a given employee ID
-"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__ == "__main":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    employee_id = int(sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        user_data = requests.get(user_url).json()
-        todos_data = requests.get(todos_url).json()
-
-        employee_name = user_data.get("name")
-        total_tasks = len(todos_data)
-        completed_tasks = [task for task in todos_data if task.get("completed")]
-
-        print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-
-        for task in completed_tasks:
-            print(f"\t {task.get('title')}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
