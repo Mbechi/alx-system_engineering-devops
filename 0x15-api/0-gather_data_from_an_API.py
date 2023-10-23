354ccf7c1c5f4209cb4dd5c3c395515c54566838
mbechi <agnessmminaa@gmail.com>
2023-10-23 23:23:26 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python3
-"""Returns to-do list information for provided employee ID."""
-import requests
+"""script using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests as r
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+"""
+This script retrieves information about a given
+employee's TODO list progress using a REST API.
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+Usage:
+    python script.py [employee_id]
+
+Args:
+    employee_id (int): The ID of the employee
+    to retrieve TODO list progress for.
+
+Example:
+    python script.py 1
+
+Inputs:
+    sys.argv[1] (str): The employee ID passed as a command line argument.
+
+Outputs:
+    The titles of the completed tasks.
+    A summary message that includes the employee's name,
+    the number of completed tasks, and the total number of tasks.
+"""
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+#    print(to_do)
+    completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
+    print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
+    [print("\t {}".format(title)) for title in completed]
