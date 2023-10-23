4e49fbaef42059444d4409a7422c22290f942dff
mbechi <agnessmminaa@gmail.com>
2023-10-23 23:19:01 +0000

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-"""Exports to-do list info for given employee ID to CSV format."""
+"""Script to export data in the CSV format"""
 import csv
-import requests
+import requests as r
 import sys
+
+"""
+This script exports user information and to-do list
+from a REST API and writes them to a CSV file.
+
+Usage:
+    python script.py <user_id>
+
+Inputs:
+    user_id (str): The ID of the user for whom the data will be exported.
+
+Outputs:
+    None. The script writes the data to a CSV file but does not '
+    return any value.
+"""
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    usr = r.get(url + "users/{}".format(user_id)).json()
+    username = usr.get("username")
+    to_do = r.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+        [writer.writerow([user_id, username, elm.get("completed"),
+                          elm.get("title")]) for elm in to_do]
