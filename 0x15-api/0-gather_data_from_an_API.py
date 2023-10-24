3f497fba0eaaa1202abe02f7e77c6bf75c95f6a4
mbechi <agnessmminaa@gmail.com>
2023-10-24 00:06:25 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,42 +1,55 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Returns to-do list information for a given employee ID and performs checks.
+"""
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <employee_id>")
-        sys.exit(1)
-
-    url = "https://jsonplaceholder.typicode.com/"
-    employee_id = sys.argv[1]
-
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
-
-    # Get the user name
-    user_name = user.get("name")
-
-    # Get the list of task titles for completed tasks
-    completed_tasks = [t.get("title") for t in todos if t.get("completed")]
-
-    # Check the formatting of the first line
-    first_line = "Employee {} is done with tasks({}/{}):".format(
-        user_name, len(completed_tasks), len(todos))
-
-    # Check if user name matches
-    if "Employee Name: Incorrect" in first_line:
+# Function to retrieve user name
+def get_user_name(user_id):
+    resp = requests.get(users_url + str(user_id))
+    
+    # Check the HTTP response status code
+    if resp.status_code != 200:
         print("Employee Name: Incorrect (Expected: Employee Name: OK)")
-    else:
-        print("Employee Name: OK")
+        return None
+    
+    try:
+        name = resp.json().get("name")
+        return name
+    except ValueError:
+        print("Employee Name: Incorrect (Expected: Employee Name: OK)")
+        return None
 
-    # Check if task count matches
-    if "To Do Count: Incorrect" in first_line:
+# Function to retrieve TODO list information
+def get_todo_list(user_id):
+    resp = requests.get(todos_url, params={"userId": user_id})
+    
+    # Check the HTTP response status code
+    if resp.status_code != 200:
         print("To Do Count: Incorrect (Expected: To Do Count: OK)")
-    else:
-        print("To Do Count: OK")
+        return []
 
-    # Check if first line formatting matches
+    try:
+        return resp.json()
+    except ValueError:
+        print("To Do Count: Incorrect (Expected: To Do Count: OK)")
+        return []
+
+# Function to check student output for correctness
+def check_student_output(user_name, todo_list):
+    completed_tasks = [t['title'] for t in todo_list if t['completed']]
+    
+    # Check if the user name is retrieved
+    if user_name is not None:
+        print("Employee Name: OK")
+    else:
+        return
+
+    # Check the first line formatting
+    first_line = "Employee {} is done with tasks({}/{}):".format(
+        user_name, len(completed_tasks), len(todo_list))
+    
     if "First line formatting: Incorrect" in first_line:
         print("First line formatting: Incorrect (Expected: First line formatting: OK)")
     else:
@@ -59,3 +72,17 @@ if __name__ == "__main__":
                 print("{} Incorrect (Expected: {} OK)".format(task_label, task_label))
         else:
             break
+
+if __name__ == "__main__":
+    users_url = "https://jsonplaceholder.typicode.com/users/"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    user_name = get_user_name(employee_id)
+    todo_list = get_todo_list(employee_id)
+    check_student_output(user_name, todo_list)
